1ace35a34db3082495c97da92aa391f31d9ec2ec
Yannick Tamerius <y.r.tamerius@students.uu.nl>
2020-02-11 18:27:33 +0100

    first day of code

---
+++ settings.py
@@ -0,0 +1,8 @@
+settings = {
+	"uniformCrossover": True,
+	"fitnessFunction": "countOnes",# countOnes, linkedDeceptiveTF, linkedDonDeceptiveTF, deceptiveNonlinkedTF, nonDeceptiveNonlinkedTF
+	"iterations": 10000,
+
+	# double, and then alter in steps of 10
+	"populationSize": 10,
+}
