dad74210f1454b4705658611d5e31c2df3bbdfa4
Yannick Tamerius <y.r.tamerius@students.uu.nl>
2020-02-13 21:19:42 +0100

    zoals we 'm achterlieten op dag 2

--- main.py
+++ main.py
@@ -119,64 +119,112 @@ def twoPointCrossover(parent1, parent2):
 		child2 += parent2[index]
 	return child1, child2
 
-population = generatePopulation(N=settings["populationSize"], length=100)
+def isGlobalOptimum(population):
+	for individual in population:
+		if "0" in individual:
+			return False
+	return True
 
-# Main algorithm
-for generation in range(0, settings["iterations"]):
-	# 1. Randomly shuffle the population P(t).
-	random.shuffle(population)
+def findN():
+	Ns = [10]
 
-	# 2. Pair solution 1 with solution 2, solution 3 with solution 4, etc. ...
-	winners = []
-	for parentIndex in range(0, len(population)):
-		if parentIndex % 2 == 0:
-			continue
-		parent1 = population[parentIndex - 1]
-		parent2 = population[parentIndex]
-
-		# 3. Each parent pair creates 2 offspring solutions using crossover.
-		if settings["uniformCrossover"]:
-			child1, child2 = uniformCrossover(parent1, parent2)
+	# search until 1280
+	while Ns[len(Ns) - 1] < 1280:
+		print(Ns[len(Ns) - 1])
+		# double after every unreliable result
+		while not checkReliability(Ns=Ns):
+			Ns.append(Ns[len(Ns) - 1] * 2)
 		else:
-			child1, child2 = twoPointCrossover(parent1, parent2)
-		
-		# 4. Family competition: the best 2 solutions of each family of 4 are copied to the next population P(t + 1).
-		fitness = {
-			parent1: None,
-			parent2: None,
-			child1: None,
-			child2: None
-		}
-		for individual in fitness.keys():
-			if settings["fitnessFunction"] == "countOnes":
-				fitness[individual] = countOnes(individual)
-			elif settings["fitnessFunction"] == "linkedDeceptiveTF":
-				fitness[individual] = linkedDeceptiveTF(individual)
-			elif settings["fitnessFunction"] == "linkedDonDeceptiveTF":
-				fitness[individual] = linkedDonDeceptiveTF(individual)
-			elif settings["fitnessFunction"] == "deceptiveNonlinkedTF":
-				fitness[individual] = deceptiveNonlinkedTF(individual)
-			elif settings["fitnessFunction"] == "nonDeceptiveNonlinkedTF":
-				fitness[individual] = nonDeceptiveNonlinkedTF(individual)
+			# only take steps of 10
+			while Ns[len(Ns) - 1] % 10 == 0:
+				if checkReliability(Ns=Ns):
+					if Ns[len(Ns) - 1] < Ns[len(Ns) - 2]:
+						Ns.append((Ns[len(Ns) - 1] + Ns[len(Ns) - 3]) / 2)
+					else:
+						Ns.append((Ns[len(Ns) - 1] + Ns[len(Ns) - 2]) / 2)
+				else:
+					if Ns[len(Ns) - 1] < Ns[len(Ns) - 2]:
+						Ns.append((Ns[len(Ns) - 1] + Ns[len(Ns) - 2]) / 2)
+					else:
+						Ns.append((Ns[len(Ns) - 1] + Ns[len(Ns) - 3]) / 2)
+			print("Stopped at N=", Ns[len(Ns) - 1])
+			return Ns[len(Ns) - 1]
+	else:
+		print("Report FAIL. Global optimum not found with size 1280.")
+		return "Fail"
+
+
+def checkReliability(Ns):
+	N = Ns[len(Ns) - 1]
+	optima = 0
+	for iteration in range(0, 25):
+		population = evolve(N=N)
+		if isGlobalOptimum(population):
+			optima += 1
+	print(Ns)
+	return optima >= 24
+
+# Generate a population of size N and evolve it.
+def evolve(N):
+	population = generatePopulation(N=N, length=100)
+	for generation in range(0, settings["generations"]):
+		# 1. Randomly shuffle the population P(t).
+		random.shuffle(population)
+
+		# 2. Pair solution 1 with solution 2, solution 3 with solution 4, etc. ...
+		winners = []
+		for parentIndex in range(0, len(population)):
+			if parentIndex % 2 == 0:
+				continue
+			parent1 = population[parentIndex - 1]
+			parent2 = population[parentIndex]
+
+			# 3. Each parent pair creates 2 offspring solutions using crossover.
+			if settings["uniformCrossover"]:
+				child1, child2 = uniformCrossover(parent1, parent2)
 			else:
-				print("No fitness function found!")
-		# Find key (the individual) of the highest fitness
-		winner = max(fitness, key=fitness.get)
-		# iff the winner is a parent: check if there exists a child with the same fitness (to use this instead)
-		if winner in [parent1, parent2]:
-			if fitness[child1] == fitness[winner]:
-				winner = child1
-			elif fitness[child2] == fitness[winner]:
-				winner = child2
-		fitness[winner] = 0 # set to zero so we skip this for the second winner
-		secondWinner = max(fitness, key=fitness.get)
-		if secondWinner in [parent1, parent2]:
-			if fitness[child1] == fitness[secondWinner]:
-				secondWinner = child1
-			elif fitness[child2] == fitness[secondWinner]:
-				secondWinner = child2
-		winners += [winner, secondWinner]
-	print(generation, "/", len(settings["iterations"]))
-	population = winners
-print("\n Final population:")
-print(population)
+				child1, child2 = twoPointCrossover(parent1, parent2)
+			
+			# 4. Family competition: the best 2 solutions of each family of 4 are copied to the next population P(t + 1).
+			fitness = {
+				parent1: None,
+				parent2: None,
+				child1: None,
+				child2: None
+			}
+			for individual in fitness.keys():
+				if settings["fitnessFunction"] == "countOnes":
+					fitness[individual] = countOnes(individual)
+				elif settings["fitnessFunction"] == "linkedDeceptiveTF":
+					fitness[individual] = linkedDeceptiveTF(individual)
+				elif settings["fitnessFunction"] == "linkedDonDeceptiveTF":
+					fitness[individual] = linkedDonDeceptiveTF(individual)
+				elif settings["fitnessFunction"] == "deceptiveNonlinkedTF":
+					fitness[individual] = deceptiveNonlinkedTF(individual)
+				elif settings["fitnessFunction"] == "nonDeceptiveNonlinkedTF":
+					fitness[individual] = nonDeceptiveNonlinkedTF(individual)
+				else:
+					print("No fitness function found!")
+			# Find key (the individual) of the highest fitness
+			winner = max(fitness, key=fitness.get)
+			# iff the winner is a parent: check if there exists a child with the same fitness (to use this instead)
+			if winner in [parent1, parent2]:
+				if fitness[child1] == fitness[winner]:
+					winner = child1
+				elif fitness[child2] == fitness[winner]:
+					winner = child2
+			fitness[winner] = 0 # set to zero so we skip this for the second winner
+			secondWinner = max(fitness, key=fitness.get)
+			if secondWinner in [parent1, parent2]:
+				if fitness[child1] == fitness[secondWinner]:
+					secondWinner = child1
+				elif fitness[child2] == fitness[secondWinner]:
+					secondWinner = child2
+			winners += [winner, secondWinner]
+		# print(generation, "/", settings["generations"])
+		population = winners
+	# print("\n Final population with N=", str(N), ":")
+	# print(population)
+	return population
+
+findN()
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 settings = {
 	"uniformCrossover": True,
 	"fitnessFunction": "countOnes",# countOnes, linkedDeceptiveTF, linkedDonDeceptiveTF, deceptiveNonlinkedTF, nonDeceptiveNonlinkedTF
-	"iterations": 10000,
+	"generations": 10,
 
 	# double, and then alter in steps of 10
 	"populationSize": 10,
